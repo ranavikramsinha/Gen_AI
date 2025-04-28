8756f304fa6252aabe4d0998c66f776345ac5a97
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-04-28 15:44:38 +0530

    learn about how to use ollama and docker to use and run open source's modal

---
+++ class_03/ollama_api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from ollama import Client
+from fastapi import Body
+
+app = FastAPI()
+
+client = Client(
+    host="http://localhost:11434",
+)
+
+client.pull("gemma3:1b")
+
+@app.post("/chat")
+
+def chat(message: str = Body(..., description="Chat Message")):
+    response = client.chat(model="gemma3:1b", messages=[
+        {"role": "user", "content": "hey there"}
+    ])
+
+    return response["message"]["content"]
